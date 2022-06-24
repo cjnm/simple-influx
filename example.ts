09ed01a814c4996a0350813c92db6852a419f719
cjnm <seejan.raj@live.com>
2022-06-25 03:02:29 +0545

    example updated

--- example.ts
+++ example.ts
@@ -1,33 +1,37 @@
 import InfluxLib, { WriteDataType } from "./index";
 
-let influx = new InfluxLib(
-  "http://localhost:8086",
-  "==", // token
-  "", // organization
-  "" //bucket
-);
+async function go() {
+  let influx = new InfluxLib(
+    "http://...:8086",
+    "==", // token
+    "", // organization
+    "" //bucket
+  );
 
-let data: WriteDataType = [
-  {
-    tag: {
-      server: "server1x",
+  let data: WriteDataType = [
+    {
+      tag: {
+        server: "server1x",
+      },
+      data: {
+        cpu: "27%",
+        memory: "80%",
+      },
     },
-    data: {
-      cpu: "27%",
-      memory: "80%",
+    {
+      tag: {
+        server: "server1x",
+      },
+      data: {
+        cpu: "28%",
+        memory: "80%",
+      },
     },
-  },
-  {
-    tag: {
-      server: "server1x",
-    },
-    data: {
-      cpu: "28%",
-      memory: "80%",
-    },
-  },
-];
+  ];
 
-influx.write("server_metrix", data);
+  // influx.write("server_metrix", data);
 
-influx.read("server_metrix", ["!memory", "cpu"], "-1h");
+  console.log(await influx.read("server_metrix", ["!memory", "cpu"], "-1h"));
+}
+
+go();
