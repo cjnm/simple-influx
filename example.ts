4701f58e252c38a9af973775bf862db590402828
cjnm <seejan.raj@live.com>
2022-06-25 01:10:27 +0545

    updated

---
+++ example.ts
@@ -0,0 +1,31 @@
+import InfluxLib, { WriteDataType } from "./index";
+
+let influx = new InfluxLib(
+  "http://localhost:8086",
+  "", // token
+  "", // organization
+  "" //bucket
+);
+
+let data: WriteDataType = [
+  {
+    tag: {
+      server: "server1x",
+    },
+    data: {
+      cpu: "27%",
+      memory: "80%",
+    },
+  },
+  {
+    tag: {
+      server: "server1x",
+    },
+    data: {
+      cpu: "28%",
+      memory: "80%",
+    },
+  },
+];
+
+influx.write("server_metrix", data);
